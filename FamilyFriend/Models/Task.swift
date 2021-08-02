32f33260ba4b64847b545b1092c049c700993b15
Dawid Nadolski <dawidnadolski@icloud.com>
2021-08-02 16:26:00 +0200

    Add model for tasks and members

---
+++ FamilyFriend/Models/Task.swift
@@ -0,0 +1,15 @@
+//
+//  Task.swift
+//  FamilyFriend
+//
+//  Created by Dawid Nadolski on 31/07/2021.
+//
+
+import Foundation
+
+struct Task {
+	let taskID: Int
+	let name: String
+	let description: String?
+	let xpPoints: Int
+}
