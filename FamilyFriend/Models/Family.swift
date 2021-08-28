fc8f444d1370222c8b4a85c90ca96da8c33fb877
Dawid Nadolski <dawidnadolski@icloud.com>
2021-08-28 17:22:28 +0200

    Add family model

---
+++ FamilyFriend/Models/Family.swift
@@ -0,0 +1,14 @@
+//
+//  Family.swift
+//  FamilyFriend
+//
+//  Created by Dawid Nadolski on 28/08/2021.
+//
+
+import Foundation
+
+struct Family {
+	let id: UUID
+	let name: String
+	let password: String
+}
